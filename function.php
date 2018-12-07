d16f150df3496a8cba4d89e505e321e47b29be10
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2018-12-07 08:51:30 +0330

    this is the main code

---
+++ function.php
@@ -0,0 +1,17 @@
+function rng_shortcode_SHORTCODENAME( $atts, $content ) {
+    //ATTRIBUTE
+  $array_atts = shortcode_atts(
+  array(
+    'PARAM'   => '',
+  ), $atts, 'SHORTCODENAME'
+    );
+    //USE PARAMS
+    $atts['PARAM'];
+    //USE CONTENT
+    $content;
+    //RETURN VALUE
+    ob_start();
+    $outpout = ob_get_clean();
+  return $outpout;
+}
+add_shortcode( 'SHORTCODENAME', 'rng_shortcode_SHORTCODENAME' );
