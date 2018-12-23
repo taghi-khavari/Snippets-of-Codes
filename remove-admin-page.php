eba0db9ef04e542279ad8905df78da84d8ceaa17
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2018-12-23 15:18:23 +0330

    Update remove-admin-page.php

--- remove-admin-page.php
+++ remove-admin-page.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action( 'admin_menu', 'weblandtk_remove_menu_pages' );
+add_action( 'admin_menu', 'weblandtk_remove_menu_pages' ,999);
 
 function weblandtk_remove_menu_pages() {
   remove_menu_page( 'edit.php' );                   //Posts
@@ -10,5 +10,11 @@ function weblandtk_remove_menu_pages() {
   remove_menu_page( 'users.php' );                  //Users
   remove_menu_page( 'tools.php' );                  //Tools
   remove_menu_page( 'options-general.php' );        //Settings
+  
+   remove_submenu_page( 'options-general.php','options-media.php' );
+   remove_submenu_page( 'options-general.php','options-discussion.php' );
+   remove_submenu_page( 'options-general.php', 'akismet-key-config' );
+   remove_submenu_page( 'admin.php', 'wp_mailjet_options_campaigns_menu' );
 
-};
+}
+
