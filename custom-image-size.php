16bde35cd47acf0d410581748b32ddae5ba97bc8
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2018-12-26 10:44:41 +0330

    Update custom-image-size.php

--- custom-image-size.php
+++ custom-image-size.php
@@ -4,3 +4,12 @@ function weblandtk_custom_image_size() {
   //  add_image_size( 'category-thumb', 300 ); // 300 pixels wide (and unlimited height)
     add_image_size( 'homepage-thumb', 207, 166, true ); // (cropped)
 }
+
+
+// Register the useful image size for use in Add Media modal
+add_filter( 'image_size_names_choose', 'weblandtk_custom_size' );
+function weblandtk_custom_size( $sizes ) {
+    return array_merge( $sizes, array(
+        'homepage-thumb' => __( 'Homepage thumbnail' )
+    ) );
+}
