e99f54978af9e764e7fcff4a7b2f4eb0005a535c
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2019-01-06 11:57:48 +0330

    Update removing-updates-notifications.php

--- removing-updates-notifications.php
+++ removing-updates-notifications.php
@@ -1,9 +1,9 @@
 <?php 
 // You can comment any of the filters below to be more specific about what you want to hide
 // hide update notifications
-function remove_core_updates(){
+function weblandtk_remove_core_updates(){
    global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
 }
-add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
-add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
-add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
+add_filter('pre_site_transient_update_core','weblandtk_remove_core_updates'); //hide updates for WordPress itself
+add_filter('pre_site_transient_update_plugins','weblandtk_remove_core_updates'); //hide updates for all plugins
+add_filter('pre_site_transient_update_themes','weblandtk_remove_core_updates'); //hide updates for all themes
