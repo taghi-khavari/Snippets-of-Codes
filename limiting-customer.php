f48c864e5fbf4612f070dca32793cb686c8da557
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2019-02-06 09:22:03 +0330

    Create limiting-customer.php

---
+++ limiting-customer.php
@@ -0,0 +1,37 @@
+<?php
+function weblandtk_simple_role_caps()
+{
+   // gets the role object
+   $role = get_role('administrator');
+
+   $role->remove_cap('delete_plugins');
+   $role->remove_cap('delete_themes');
+   $role->remove_cap('install_plugins');
+   $role->remove_cap('install_themes');
+   $role->remove_cap('edit_plugins');
+   $role->remove_cap('switch_themes');
+}
+
+// add or remove capabilities, priority must be after the initial role definition
+add_action('init', 'weblandtk_simple_role_caps', 11);
+
+
+
+
+// You can comment any of the filters below to be more specific about what you want to hide
+// hide update notifications
+function remove_core_updates(){
+   global $wp_version;return(object) array('last_checked'=> time(),'version_checked'=> $wp_version,);
+}
+add_filter('pre_site_transient_update_core','remove_core_updates'); //hide updates for WordPress itself
+add_filter('pre_site_transient_update_plugins','remove_core_updates'); //hide updates for all plugins
+add_filter('pre_site_transient_update_themes','remove_core_updates'); //hide updates for all themes
+
+
+function weblandtk_adjust_the_wp_menu() {
+   $page = remove_submenu_page( 'index.php', 'update-core.php' );
+   // $page[0] is the menu title
+   // $page[1] is the minimum level or capability required
+   // $page[2] is the URL to the item's file
+}
+add_action( 'admin_menu', 'weblandtk_adjust_the_wp_menu', 999 );
