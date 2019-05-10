07a65b6584a5f6b7a33e72126d870a1dbcad1394
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2019-05-10 11:32:04 +0430

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -1,7 +1,14 @@
+ // Enable WP_DEBUG mode
 define( 'WP_DEBUG', true ); 
+
 if ( WP_DEBUG ) {
+    // Enable Debug logging to the /wp-content/debug.log file
     define( 'WP_DEBUG_LOG', true );
+
+    // Disable display of errors and warnings 
     define( 'WP_DEBUG_DISPLAY', false );
+
+    // Use dev versions of core JS and CSS files (only needed if you are modifying these core files)
     @ini_set( 'display_errors', 0 );
 }
 
