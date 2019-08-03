607e37a9c456e129b5a93af6233fbf4fa067429f
taghikhavari <44890399+taghikhavari@users.noreply.github.com>
2019-08-03 10:14:12 +0430

    Create wp_pagination.php

---
+++ wp_pagination.php
@@ -0,0 +1,25 @@
+function weblandtk_pagination($prev = '<i class="fa fa-angle-double-left"></i>', $next = '<i class="fa fa-angle-double-right"></i>', $pages='') {
+    global $wp_query, $wp_rewrite;
+    $wp_query->query_vars['paged'] > 1 ? $current = $wp_query->query_vars['paged'] : $current = 1;
+    if($pages==''){
+        global $wp_query;
+        $pages = $wp_query->max_num_pages;
+        if(!$pages)
+        {
+            $pages = 1;
+        }
+    }
+    $pagination = array(
+		'base' 			=> str_replace( 999999999, '%#%', get_pagenum_link( 999999999 ) ),
+		'format' 		=> '',
+		'current' 		=> max( 1, get_query_var('paged') ),
+		'total' 		=> $pages,
+		'prev_text' => $prev,
+        'next_text' => $next,		
+        'type'			=> 'list',
+		'end_size'		=> 3,
+		'mid_size'		=> 3
+    );
+    $return =  paginate_links( $pagination );
+	echo str_replace( "<ul class='page-numbers'>", '', $return );
+}
